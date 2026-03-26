bd73d2c69d7341f29a0ff9f50debf5625697145a
djamel-ouazib <djamel.ouazibb@gmail.com>
2026-03-26 18:32:29 +0100

    Feat: add vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from 'next-themes'
-
+import { Analytics } from '@vercel/analytics/next'
 const geistSans = Geist({
     variable: '--font-geist-sans',
     subsets: ['latin'],
@@ -28,6 +28,7 @@ export default function RootLayout({
             <body className="min-h-full flex flex-col">
                 <ThemeProvider attribute="class">{children}</ThemeProvider>
             </body>
+            <Analytics />
         </html>
     )
 }
